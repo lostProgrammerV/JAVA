9dc4396fe2f82e503d6011dc89f4c7f774d1645a
Programmer Vini <100801106+lostProgrammerV@users.noreply.github.com>
2022-11-14 15:41:25 -0300

    atualiza familia

--- Lists/finalChallenge/jogoDaVelha.java
+++ Lists/finalChallenge/jogoDaVelha.java
@@ -1,80 +1,94 @@
 package Lists.finalChallenge;
 
-import java.util.Vector;
-
 import javax.swing.JOptionPane;
 
 public class jogoDaVelha {
-    String[][] posicao = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
-
     public static void main(String[] args) {
-        jogoDaVelha jg = new jogoDaVelha();
-    }
+        int cont = 0;
+        int pos = 0;
+        String xO;
 
-    public String mostraJogada() {
-        for (int c = 0; c < 3; c++) {
-            for (int d = 0; d < 3; d++) {
-                JOptionPane.showMessageDialog(null, "  " + posicao[c][d]);
+        String[][] tabuleiro = new String[3][3];
+
+        for (int d = 0; d < 3; d++) {
+            for (int c = 0; c < 3; c++) {
+                cont += 1;
+                tabuleiro[d][c] = "" + cont;
             }
         }
-        return null;
+
+        String nomePlayer = JOptionPane.showInputDialog("Qual o nome do player 1");
+        String nomePlayer2 = JOptionPane.showInputDialog("Qual o nome do player 2");
+
+        do {
+            cont += 1;
+
+            if (cont % 2 == 0) {
+                xO = "x";
+            } else {
+                xO = "o";
+            }
+
+            mostraTabuleiro(tabuleiro);
+            if (cont % 2 == 0) {
+                pos = Integer.parseInt(
+                        JOptionPane.showInputDialog("Jogador(a) " + nomePlayer + " escolha uma posi��o (1 a 9)"));
+            } else {
+                pos = Integer.parseInt(
+                        JOptionPane.showInputDialog("Jogador(a) " + nomePlayer2 + " escolha uma posi��o (1 a 9)"));
+            }
+
+            marcaPos(pos, tabuleiro, cont, xO);
+            vitorias(tabuleiro, cont, nomePlayer, nomePlayer2);
+        } while (true);
     }
 
-    public boolean Valida(String posi) {
-        for (int l = 0; l < 3; l++) {
+    private static void marcaPos(int pos, String[][] tabuleiro, int cont, String xO) {
+        String sPos = Integer.toString(pos);
+        if (pos > 9 || pos < 1) {
+            JOptionPane.showMessageDialog(null,
+                    "A posi��o " + pos + " n�o existe, digite outro numero sua proxima vez");
+            return;
+        }
+
+        for (int d = 0; d < 3; d++) {
             for (int c = 0; c < 3; c++) {
-                if (posicao[c][l].equals(posi)) {
-                    return true;
+                if (sPos.contentEquals(tabuleiro[d][c])) {
+                    tabuleiro[d][c] = xO;
+                    return;
                 }
             }
         }
-        return false;
+        JOptionPane.showMessageDialog(null, "A posi��o " + pos + " est� marcada, digite outro numero sua proxima vez");
     }
 
-    public void Jogada(String posi, String x) {
-        if (posi.equals("1")) {
-            posicao[0][0] = x;
-        } else if (posi.equals("2")) {
-            posicao[0][1] = x;
-        } else if (posi.equals("3")) {
-            posicao[0][2] = x;
-        } else if (posi.equals("4")) {
-            posicao[1][0] = x;
-        } else if (posi.equals("5")) {
-            posicao[1][1] = x;
-        } else if (posi.equals("6")) {
-            posicao[1][2] = x;
-        } else if (posi.equals("7")) {
-            posicao[2][0] = x;
-        } else if (posi.equals("8")) {
-            posicao[2][1] = x;
-        } else if (posi.equals("9")) {
-            posicao[2][2] = x;
-        }
-    }
-
-    public String vitorias(int Jogadas) {
-        String[] vit1 = new String[0];
-        String vencedor = null;
-        if (Jogadas == 2) {
-            vencedor = "empate";
-        }
-        vit1[0] = ;
-        vit1[0] = ;
-        vit1[0] = ;
-        
-        vit1[0] = ;
-        vit1[0] = ;
-        vit1[0] = ;
-
-        vit1[0] = ;
-        vit1[0] = ;
-        for(int c = 0; c <vit1.length; c ++){
-            if(vit1[c].equals("xxx")){
-                vencedor = "Jogador 1";
-            }else if(vit1[c].equals("ooo")){
-                vencedor = "Jogador 2";
+    private static void vitorias(String[][] tabuleiro, int cont, String nomePlayer, String nomePlayer2) {
+        for (int d = 0; d < 3; d++) {
+            for (int c = 0; c < 3; c++) {
+                if (cont == 8) {
+                    JOptionPane.showMessageDialog(null, "deu velha");
+                    System.exit(0);
+                }
+                if (tabuleiro[d][0] == "x" && tabuleiro[d][1] == "x" && tabuleiro[d][2] == "x"
+                        || tabuleiro[0][c] == "x" && tabuleiro[1][c] == "x" && tabuleiro[2][c] == "x"
+                        || tabuleiro[d][0] == "x" && tabuleiro[1][1] == "x" && tabuleiro[c][2] == "x") {
+                    JOptionPane.showMessageDialog(null, "O jogador " + nomePlayer + " (jogador 1) venceu");
+                    System.exit(0);
+                }
+                if (tabuleiro[d][0] == "o" && tabuleiro[d][1] == "o" && tabuleiro[d][2] == "o"
+                        || tabuleiro[0][c] == "o" && tabuleiro[1][c] == "o" && tabuleiro[2][c] == "o"
+                        || tabuleiro[d][0] == "o" && tabuleiro[1][1] == "o" && tabuleiro[c][2] == "o") {
+                    JOptionPane.showMessageDialog(null, "O jogador " + nomePlayer2 + " (jogador 2) venceu");
+                    System.exit(0);
+                }
             }
         }
+    }
+
+    private static void mostraTabuleiro(String[][] tabuleiro) {
+        JOptionPane.showMessageDialog(null,
+                tabuleiro[0][0] + " " + tabuleiro[0][1] + " " + tabuleiro[0][2] + "\n" + tabuleiro[1][0] + " "
+                        + tabuleiro[1][1] + " " + tabuleiro[1][2] + "\n" + tabuleiro[2][0] + " " + tabuleiro[2][1] + " "
+                        + tabuleiro[2][2] + "\n");
     }
 }
